7a468d836b29837ad91c6c92c985ea415f6f61d2
EmilieLamps <emilie.lamps@gmail.com>
2019-10-24 14:38:58 +0200

    Premier envoi

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+
+function division() {
+let firstNumber = document.getElementById("firstNumber").value;
+let secondNumber = document.getElementById("secondNumber").value;
+let result = firstNumber / secondNumber;
+  alert(result);
+}
